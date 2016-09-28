e22d5bdbdc09d819b1137d12d00a3feb1adb1751
Anubhav Agarwal A <anubhav.a.agarwal@ericsson.com>
2016-09-28 15:00:27 +0530

    Arduino: Yet another blinking led on breadboard

---
+++ yet_another_blinking_led/led_blink_sketch.ino
@@ -0,0 +1,15 @@
+#define LED 9
+
+void setup() {
+  // the following code would run only once, before the loop() starts
+  pinMode(LED, OUTPUT);
+
+}
+
+void loop() {
+  // the following code would run repeatedly:
+  digitalWrite(LED, HIGH);
+  delay(100);
+  digitalWrite(LED, LOW);
+  delay(100);
+}
